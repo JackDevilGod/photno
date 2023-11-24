92db5d32a5056264bbe566c9ff3eb2f22c45a917
JackDevilGod <151947211+JackDevilGod@users.noreply.github.com>
2023-11-24 23:00:11 +0100

    added chatgpt code for image compare still need to figure out how it works

--- image compare.py
+++ image compare.py
@@ -1,1 +1,15 @@
-print()
+from skimage import metrics
+from skimage import io
+
+
+def compare_ssim(image1, image2):
+    img1 = io.imread(image1)
+    img2 = io.imread(image2)
+
+    # Use structural_similarity() function
+    similarity = metrics.structural_similarity(img1, img2)
+    return similarity
+
+
+print(compare_ssim(r"",
+                   r""))
