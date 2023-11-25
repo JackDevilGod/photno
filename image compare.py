a18813e80ae6524f1887c67092a0765958f5721a
JackDevilGod <151947211+JackDevilGod@users.noreply.github.com>
2023-11-25 20:13:38 +0100

    will this work for file formatter as 1-00000+x.png

--- image compare.py
+++ image compare.py
@@ -1,25 +1,36 @@
 # import required libraries
 import cv2
 import numpy as np
+import os
 
-n = 0
-# load the input images
-img1 = cv2.imread('1-' + "".join(["0" for _ in range(4-n)]) + str(n) + '.png')
-img2 = cv2.imread('1-' + "".join(["0" for _ in range(4-n)]) + str(n) + '.png')
 
-# convert the images to grayscale
-img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+def mse(first, second):
+    # load the input images
+    img1 = cv2.imread('1-' + "".join(["0" for _ in range(4 - first)]) + str(first) + '.png')
+    img2 = cv2.imread('1-' + "".join(["0" for _ in range(4 - second)]) + str(second) + '.png')
 
-# define the function to compute MSE between two images
-def mse(img_1, img_2):
+    # convert the images to grayscale
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+    img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+
     h, w = img1.shape
-    diff = cv2.subtract(img_1, img_2)
+    diff = cv2.subtract(img1, img2)
     err = np.sum(diff**2)
     meanse = err/(float(h*w))
     return meanse
 
 
+amount_picutes = int(input())
+count = 1
+offset = 1
 # 50% match is 124.5
-error = mse(img1, img2)
-print(error)
+while count < amount_picutes:
+    if mse(count, count + offset) < 124.5:
+        os.remove('1-' + "".join(["0" for _ in range(4 - count)]) + str(count) + '.png')
+        offset += 1
+    else:
+        count = count + offset
+        offset = 1
+
+    if count + offset == amount_picutes:
+        count = amount_picutes
