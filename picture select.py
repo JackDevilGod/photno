57897728c231755793a115e58fe9d7539c50bc12
JackDevilGod <151947211+JackDevilGod@users.noreply.github.com>
2023-12-09 04:06:35 +0100

    split the functions into another file

--- picture select.py
+++ picture select.py
@@ -3,11 +3,11 @@
 import os
 from functions import *
 
-amount_picutes = int(input("how many images are there"))
+amount_picture = int(input("how many images are there"))
 count = 1
 offset = 1
-# 50% match is 124.5
-while count < amount_picutes:
+# 50% match is 124.5 histogram
+while count < amount_picture:
     if mse(count, count + offset) < 25 and histogram(count, count + offset) < 50:
         os.remove('1-' + "".join(["0" for _ in range(5 - len(str(count + offset)))]) + str(count + offset) + '.png')
         offset += 1
@@ -15,5 +15,5 @@ while count < amount_picutes:
         count = count + offset
         offset = 1
 
-    if count + offset >= amount_picutes:
-        count = amount_picutes
+    if count + offset >= amount_picture:
+        count = amount_picture
